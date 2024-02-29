420006ca57f728798fafcc04489a9970a46fdf7e
1ilit <lilitmousheghyan@gmail.com>
2024-02-29 16:14:41 +0200

    Add vercel anaytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-// import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { LocaleProvider } from "@douyinfe/semi-ui";
+import { Analytics } from "@vercel/analytics/react";
+
 import en_US from "@douyinfe/semi-ui/lib/es/locale/source/en_US";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-   <LocaleProvider locale={en_US}>
-      <App />
-    </LocaleProvider>
+  <LocaleProvider locale={en_US}>
+    <App />
+    <Analytics />
+  </LocaleProvider>
   // </React.StrictMode>
 );
