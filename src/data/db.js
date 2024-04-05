eff3f570717858e2861e2a1dfd585298897d0177
1ilit <lilitmousheghyan@gmail.com>
2024-04-05 03:08:00 +0300

    Bump up db version

--- src/data/db.js
+++ src/data/db.js
@@ -3,7 +3,7 @@ import { templateSeeds } from "./seeds";
 
 export const db = new Dexie("drawDB");
 
-db.version(4).stores({
+db.version(5).stores({
   diagrams: "++id, lastModified",
   templates: "++id, custom",
 });
