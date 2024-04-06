2c525a2ffd697ae5c78f3e461e552813b9969262
1ilit <lilitmousheghyan@gmail.com>
2024-04-06 11:11:12 +0300

    Add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "prettier"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
