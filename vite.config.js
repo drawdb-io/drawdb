590a340de08d7e842619ad8bc0d8009e1a1f1d20
haecheonlee <dlgocjs1@gmail.com>
2024-05-05 22:12:56 -0400

    enh: set eslint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
 })
