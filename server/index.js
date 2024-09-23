d1d23b1c9209aed540b60fe9f0541c882ca93d5d
Tei Yuan Wei <yuanwei92@gmail.com>
2024-09-23 16:24:39 +0800

    use ddb files as source

--- server/index.js
+++ server/index.js
@@ -14,14 +14,14 @@ app.use(express.json())
 // Serve the static files from the DrawDB app
 app.use(express.static(DRAWDB_HOME));
 
+// Endpoints
 app.post('/api/usercode-files/', (req, res) => {
   const { filename, content } = req.body;
-
   if (!filename || !content) {
       return res.status(400).send('Filename and content are required');
   }
-  const filePath = path.join(DRAWDB_FILE_DIR, filename);
 
+  const filePath = path.join(DRAWDB_FILE_DIR, filename);
   fs.writeFile(filePath, JSON.stringify(content), 'utf8', (err) => {
       if (err) {
           return res.status(500).send('Error writing ddb file');
@@ -32,15 +32,13 @@ app.post('/api/usercode-files/', (req, res) => {
 
 app.get('/api/usercode-files/:filename', (req, res) => {
   const filename = req.params.filename;
-  const filePath = path.join(DRAWDB_FILE_DIR, filename); // Adjust the path based on your directory structure
+  const filePath = path.join(DRAWDB_FILE_DIR, filename);
 
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       if (err.code === 'ENOENT') {
-        // File not found
         return res.status(404).send('File not found');
       }
-      // Some other error
       return res.status(500).send('Error reading file');
     }
     res.send(data);
@@ -49,13 +47,11 @@ app.get('/api/usercode-files/:filename', (req, res) => {
 
 app.delete('/api/usercode-files/:filename', (req, res) => {
   const filename = req.params.filename;
-
   if (!filename) {
     return res.status(400).send('Filename is required');
   }
 
   const filePath = path.join(DRAWDB_FILE_DIR, filename);
-
   fs.access(filePath, fs.constants.F_OK, (err) => {
     if (err) {
       return res.send('File does not exists.');
@@ -69,11 +65,25 @@ app.delete('/api/usercode-files/:filename', (req, res) => {
   });
 })
 
-// Handles any requests that don't match the ones above
+app.get('/api/diagrams', (_req, res) => {
+  const dirPath = DRAWDB_FILE_DIR;
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      return res.status(500).send('Unable to scan directory');
+    }
+    const ddbFiles = files.filter(file => path.extname(file) === '.ddb');
+    const fileContents = ddbFiles.map(file => {
+      const filePath = path.join(dirPath, file);
+      const fileContent = fs.readFileSync(filePath, 'utf-8');
+      return JSON.parse(fileContent);
+    });
+    res.json(fileContents);
+  });
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(DRAWDB_HOME, 'index.html'));
 });
-
 
 app.listen(DRAWDB_PORT, () => {
   console.log(`DrawDB is running on port ${DRAWDB_PORT}`);
--- src/data/db.js
+++ src/data/db.js
@@ -1,7 +1,8 @@
 import Dexie from "dexie";
 import 'dexie-observable';
 import { templateSeeds } from "./seeds";
-import { diagramToDrawDbFile } from "../utils/parser"
+import { diagramToDdbFile, ddbFileToDiagram, writeDdbFiles, deleteDdbFiles } from "./usercode"
+import { ddbDiagramIsValid } from "../utils/validateSchema";
 
 export const db = new Dexie("drawDB");
 
@@ -14,10 +15,27 @@ db.on("populate", (transaction) => {
   transaction.templates.bulkAdd(templateSeeds).catch((e) => console.log(e));
 });
 
+db.on("ready", async (db) => {
+  // Use ddb files as source for diagrams
+  const diagramsRes = await fetch('/api/diagrams', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  })
+  const ddbFiles = await diagramsRes.json();
+  const validDdbFiles = ddbFiles.filter(ddbFile => ddbDiagramIsValid(ddbFile));
+  const diagrams = validDdbFiles.map(f => ddbFileToDiagram(f))
+  return db.transaction('rw', db.diagrams, async () => {
+    await db.diagrams.clear();
+    await db.diagrams.bulkAdd(diagrams);
+  })
+})
 
 const debounce = (func, delay) => {
   let timer;
-  return function(...args) {
+  return function (...args) {
     clearTimeout(timer);
     timer = setTimeout(() => {
       func.apply(this, args);
@@ -30,24 +48,30 @@ const debouncedChangesHandler = debounce(async (changes) => {
 }, 1500);
 
 const handleDiagramChanges = (diagramChanges) => {
-  console.log("diagramChanges", diagramChanges);
+  diagramChanges.forEach(({ type, obj, oldObj }) => {
+    const ddbFile = diagramToDdbFile(obj);
 
-  // Handle create / update / delete separately
+    switch (type) {
+      case 1:
+        writeDdbFiles([ddbFile]);
+        break;
 
-  // Parse changes to ddb file format
-  const ddbFiles = diagramChanges.map(d => diagramToDrawDbFile(d.obj));
-  
-  // Write files to usercode
-  ddbFiles.forEach(ddbFile => {
-    fetch('/api/usercode-files', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ filename: `${ddbFile.title}.ddb`, content: ddbFile })
-    });
-  });
+      case 2:
+        if (obj.id === oldObj.id && obj.name !== oldObj.name) {
+          const oldDdbFile = diagramToDdbFile(oldObj);
+          deleteDdbFiles([oldDdbFile]);
+        }
+        writeDdbFiles([ddbFile]);
+        break;
+
+      case 3:
+        deleteDdbFiles([ddbFile]);
+        break;
+
+      default:
+        break;
+    }
+  })
 }
 
 db.on('changes', debouncedChangesHandler)
