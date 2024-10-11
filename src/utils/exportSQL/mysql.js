9f834dae2bdf1364b3601f9da9a326d42b337433
1ilit <1ilit@proton.me>
2024-10-11 15:28:17 +0400

    Clean up

--- src/utils/exportSQL/mysql.js
+++ src/utils/exportSQL/mysql.js
@@ -9,27 +9,33 @@ export function toMySQL(diagram) {
         `CREATE TABLE \`${table.name}\` (\n${table.fields
           .map(
             (field) =>
-              `\t\`${field.name}\` ${field.type}${field.values ? "(" + field.values.map(value => "'" + value + "'").join(", ") + ")" : ""}${field.unsigned ? " UNSIGNED" : ""}${field.size !== undefined && field.size !== "" ? "(" + field.size + ")" : ""}${field.notNull ? " NOT NULL" : ""
-              }${field.increment ? " AUTO_INCREMENT" : ""
-              }${field.unique ? " UNIQUE" : ""}${field.default !== ""
-                ? ` DEFAULT ${parseDefault(field, diagram.database)}`
-                : ""
-              }${field.check === "" ||
+              `\t\`${field.name}\` ${field.type}${field.values ? "(" + field.values.map((value) => "'" + value + "'").join(", ") + ")" : ""}${field.unsigned ? " UNSIGNED" : ""}${field.size !== undefined && field.size !== "" ? "(" + field.size + ")" : ""}${
+                field.notNull ? " NOT NULL" : ""
+              }${
+                field.increment ? " AUTO_INCREMENT" : ""
+              }${field.unique ? " UNIQUE" : ""}${
+                field.default !== ""
+                  ? ` DEFAULT ${parseDefault(field, diagram.database)}`
+                  : ""
+              }${
+                field.check === "" ||
                 !dbToTypes[diagram.database][field.type].hasCheck
-                ? ""
-                : ` CHECK(${field.check})`
+                  ? ""
+                  : ` CHECK(${field.check})`
               }${field.comment ? ` COMMENT '${field.comment}'` : ""}`,
           )
-          .join(",\n")}${table.fields.filter((f) => f.primary).length > 0
-          ? `,\n\tPRIMARY KEY(${table.fields
-            .filter((f) => f.primary)
-            .map((f) => `\`${f.name}\``)
-            .join(", ")})`
-          : ""
+          .join(",\n")}${
+          table.fields.filter((f) => f.primary).length > 0
+            ? `,\n\tPRIMARY KEY(${table.fields
+                .filter((f) => f.primary)
+                .map((f) => `\`${f.name}\``)
+                .join(", ")})`
+            : ""
         }\n)${table.comment ? ` COMMENT='${table.comment}'` : ""};\n${`\n${table.indices
           .map(
             (i) =>
-              `\nCREATE ${i.unique ? "UNIQUE " : ""}INDEX \`${i.name
+              `\nCREATE ${i.unique ? "UNIQUE " : ""}INDEX \`${
+                i.name
               }\`\nON \`${table.name}\` (${i.fields
                 .map((f) => `\`${f}\``)
                 .join(", ")});`,
@@ -37,12 +43,15 @@ export function toMySQL(diagram) {
           .join("")}`}`,
     )
     .join("\n")}\n${diagram.references
-      .map(
-        (r) =>
-          `ALTER TABLE \`${diagram.tables[r.startTableId].name
-          }\`\nADD FOREIGN KEY(\`${diagram.tables[r.startTableId].fields[r.startFieldId].name
-          }\`) REFERENCES \`${diagram.tables[r.endTableId].name}\`(\`${diagram.tables[r.endTableId].fields[r.endFieldId].name
-          }\`)\nON UPDATE ${r.updateConstraint.toUpperCase()} ON DELETE ${r.deleteConstraint.toUpperCase()};`,
-      )
-      .join("\n")}`;
+    .map(
+      (r) =>
+        `ALTER TABLE \`${
+          diagram.tables[r.startTableId].name
+        }\`\nADD FOREIGN KEY(\`${
+          diagram.tables[r.startTableId].fields[r.startFieldId].name
+        }\`) REFERENCES \`${diagram.tables[r.endTableId].name}\`(\`${
+          diagram.tables[r.endTableId].fields[r.endFieldId].name
+        }\`)\nON UPDATE ${r.updateConstraint.toUpperCase()} ON DELETE ${r.deleteConstraint.toUpperCase()};`,
+    )
+    .join("\n")}`;
 }
