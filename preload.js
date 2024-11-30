fcf1736aab266d9562fb4ccd7d07e23ad133e0cc
DNLRQ <18170433@itculiacan.edu.mx>
2024-11-30 09:47:52 -0700

    Fix preload.js import

--- preload.js
+++ preload.js
@@ -1,4 +1,4 @@
-const { contextBridge, ipcRenderer } = require('electron');
+import { contextBridge, ipcRenderer } from 'electron';
 
 // Exponer API segura al proceso renderer
 contextBridge.exposeInMainWorld('electron', {
