08a9ff28034c0b6c95071ee246910d7e6d7c2e4f
Francisco-Galindo <yo@franciscogalindo.com>
2025-01-14 11:37:03 -0600

    Add subtype restrictions

--- src/data/constants.js
+++ src/data/constants.js
@@ -34,6 +34,13 @@ export const Cardinality = {
   MANY_TO_ONE: "many_to_one",
 };
 
+export const SubtypeRestriction = {
+  DISJOINT_TOTAL: "disjoint_total",
+  DISJOINT_PARTIAL: "disjoint_partial",
+  OVERLAPPING_TOTAL: "overlapping_total",
+  OVERLAPPING_PARTIAL: "overlapping_partial",
+};
+
 export const Constraint = {
   NONE: "No action",
   RESTRICT: "Restrict",
