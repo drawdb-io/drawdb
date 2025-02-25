6f10b687217173d6c906a4db37826e83aa97eb3f
KRATIK_BOHRA <kratikbohra5@gmail.com>
2025-02-25 13:52:54 +0530

    condition check to showDataType

--- src/components/EditorCanvas/Table.jsx
+++ src/components/EditorCanvas/Table.jsx
@@ -361,17 +361,21 @@ export default function Table(props) {
             />
           ) : (
             <div className="flex gap-1 items-center">
-              {fieldData.primary && <IconKeyStroked />}
-              {!fieldData.notNull && <span>?</span>}
-              <span>
-                {fieldData.type +
-                  ((dbToTypes[database][fieldData.type].isSized ||
-                    dbToTypes[database][fieldData.type].hasPrecision) &&
-                    fieldData.size &&
-                    fieldData.size !== ""
-                    ? "(" + fieldData.size + ")"
-                    : "")}
-              </span>
+              {showDataType && (
+                <>
+                  {fieldData.primary && <IconKeyStroked />}
+                  {!fieldData.notNull && <span>?</span>}
+                  <span>
+                    {fieldData.type +
+                      ((dbToTypes[database][fieldData.type].isSized ||
+                        dbToTypes[database][fieldData.type].hasPrecision) &&
+                        fieldData.size &&
+                        fieldData.size !== ""
+                        ? "(" + fieldData.size + ")"
+                        : "")}
+                  </span>
+                </>
+              )}
             </div>
           )}
         </div>
