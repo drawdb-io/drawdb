c02d50249e414fba211085ed75c37e644e09a268
Daniil Zhuravlev <nakrovati@outlook.com>
2025-05-03 01:39:07 +0400

    chore: use @tailwindcss/vite instead of postcss

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tailwindcss(), react()],
+});
