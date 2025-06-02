93912700c4e9a49079e77c5774c55f06a0bd3047
Javier <javierariashe@gmail.com>
2025-06-02 12:34:03 -0600

    update index details layout

--- src/components/EditorSidePanel/TablesTab/IndexDetails.jsx
+++ src/components/EditorSidePanel/TablesTab/IndexDetails.jsx
@@ -1,6 +1,6 @@
 import { Action, ObjectType } from "../../../data/constants";
-import { Input, Button, Popover, Checkbox, Select, Row, Col } from "@douyinfe/semi-ui";
-import { IconMore, IconDeleteStroked } from "@douyinfe/semi-icons";
+import { Input, Button, Checkbox, Select, Row, Col } from "@douyinfe/semi-ui";
+import { IconDeleteStroked } from "@douyinfe/semi-icons";
 import { useDiagram, useUndoRedo } from "../../../hooks";
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
@@ -12,99 +12,9 @@ export default function IndexDetails({ data, fields, iid, tid }) {
   const [editField, setEditField] = useState({});
 
   return (
-    <Row gutter={6} className="hover-1 my-2">
-      <Col span={8}>
-          <Input
-          value={data.name}
-          placeholder={t("name")}
-          validateStatus={data.name.trim() === "" ? "error" : "default"}
-          onFocus={() =>
-              setEditField({
-              name: data.name,
-              })
-          }
-          onChange={(value) =>
-              updateTable(tid, {
-              indices: tables[tid].indices.map((index) =>
-                  index.id === iid
-                  ? {
-                      ...index,
-                      name: value,
-                      }
-                  : index,
-              ),
-              })
-          }
-          onBlur={(e) => {
-              if (e.target.value === editField.name) return;
-              setUndoStack((prev) => [
-              ...prev,
-              {
-                  action: Action.EDIT,
-                  element: ObjectType.TABLE,
-                  component: "index",
-                  tid: tid,
-                  iid: iid,
-                  undo: editField,
-                  redo: { name: e.target.value },
-                  message: t("edit_table", {
-                  tableName: tables[tid].name,
-                  extra: "[index]",
-                  }),
-              },
-              ]);
-              setRedoStack([]);
-          }}
-          />
-      </Col>
-      <Col span={12}>
-        <Select
-            placeholder={t("select_fields")}
-            multiple
-            validateStatus={data.fields.length === 0 ? "error" : "default"}
-            optionList={fields}
-            className="w-full"
-            value={data.fields}
-            onChange={(value) => {
-            setUndoStack((prev) => [
-                ...prev,
-                {
-                action: Action.EDIT,
-                element: ObjectType.TABLE,
-                component: "index",
-                tid: tid,
-                iid: iid,
-                undo: {
-                    fields: [...data.fields],
-                },
-                redo: {
-                  fields: [...value],
-                },
-                message: t("edit_table", {
-                    tableName: tables[tid].name,
-                    extra: "[index field]",
-                }),
-                },
-            ]);
-            setRedoStack([]);
-            updateTable(tid, {
-                indices: tables[tid].indices.map((index) =>
-                index.id === iid
-                    ? {
-                        ...index,
-                        fields: [...value],
-                    }
-                    : index,
-                ),
-            });
-            }}
-        />
-      </Col>
-      <Col span={3}>
-        <Popover
-            content={
-            <div className="px-1 popover-theme">
-                <div className="font-semibold mb-1">{t("name")}: </div>
+    <div className="hover-1">
+        <Row gutter={6} className="my-2 flex items-center">
+            <Col span={16}>
                 <Input
                 value={data.name}
                 placeholder={t("name")}
@@ -147,7 +57,8 @@ export default function IndexDetails({ data, fields, iid, tid }) {
                     setRedoStack([]);
                 }}
                 />
-                <div className="flex justify-between items-center my-3">
+            </Col>
+            <Col span={7} className="flex justify-between items-center ml-1">
                 <div className="font-medium">{t("unique")}</div>
                 <Checkbox
                     value="unique"
@@ -189,11 +100,56 @@ export default function IndexDetails({ data, fields, iid, tid }) {
                     });
                     }}
                 ></Checkbox>
-                </div>
+            </Col>
+        </Row>
+        <Row gutter={6} className="my-2">
+            <Col span={20}>
+                <Select
+                    placeholder={t("select_fields")}
+                    multiple
+                    validateStatus={data.fields.length === 0 ? "error" : "default"}
+                    optionList={fields}
+                    className="w-full"
+                    value={data.fields}
+                    onChange={(value) => {
+                    setUndoStack((prev) => [
+                        ...prev,
+                        {
+                        action: Action.EDIT,
+                        element: ObjectType.TABLE,
+                        component: "index",
+                        tid: tid,
+                        iid: iid,
+                        undo: {
+                            fields: [...data.fields],
+                        },
+                        redo: {
+                        fields: [...value],
+                        },
+                        message: t("edit_table", {
+                            tableName: tables[tid].name,
+                            extra: "[index field]",
+                        }),
+                        },
+                    ]);
+                    setRedoStack([]);
+                    updateTable(tid, {
+                        indices: tables[tid].indices.map((index) =>
+                        index.id === iid
+                            ? {
+                                ...index,
+                                fields: [...value],
+                            }
+                            : index,
+                        ),
+                    });
+                    }}
+                />
+            </Col>
+            <Col span={3}>
                 <Button
                 icon={<IconDeleteStroked />}
                 type="danger"
-                block
                 onClick={() => {
                     setUndoStack((prev) => [
                     ...prev,
@@ -220,20 +176,9 @@ export default function IndexDetails({ data, fields, iid, tid }) {
                     });
                 }}
                 >
-                {t("delete")}
                 </Button>
-            </div>
-            }
-            trigger="click"
-            position="rightTop"
-            showArrow
-        >
-            <Button
-            icon={<IconMore />}
-            type="tertiary"
-            />
-        </Popover>
-      </Col>
-    </Row>
+            </Col>
+        </Row>
+    </div>
   );
 }
