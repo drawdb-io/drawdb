a36430f2ea2836b0557e500cc7c260f776411224
pokepato2002 <patricio.ad26301@gmail.com>
2025-06-16 22:40:04 -0600

    breakpoints

--- src/utils/calcPath.js
+++ src/utils/calcPath.js
@@ -6,17 +6,21 @@ export function calcPath(r, tableWidth = 200, zoom = 1) {
   let y2 = r.endTable.y;
 
   let radius = 10 * zoom;
-
+  const breakpoints = [];
 
   const isRecursiveLike = Math.abs(x1 - x2) < 1;
 
   if (isRecursiveLike) {
     const loopOutwardDistance = Math.max(40 * zoom, width / 4);
     const verticalMidpoint = (y1 + y2) / 2;
+    breakpoints.push(
+      { x: x1 + width + loopOutwardDistance, y: y1 },
+      { x: x1 + width + loopOutwardDistance, y: verticalMidpoint }
+    );
     const path = `M ${x1 + width} ${y1} ` +
                  `Q ${x1 + width + loopOutwardDistance} ${y1}, ${x1 + width + loopOutwardDistance} ${verticalMidpoint} ` +
                  `T ${x1 + width} ${y2}`;
-    return path;
+    return { path, breakpoints };
   }
 
   if (Math.abs(y1 - y2) <= 36 * zoom) {
@@ -24,10 +28,10 @@ export function calcPath(r, tableWidth = 200, zoom = 1) {
     if (radius <= 2) {
       if (x1 + width <= x2) {
         const path = `M ${x1 + width} ${y1} L ${x2} ${y2 + 0.1}`;
-        return path;
+        return { path, breakpoints };
       } else if (x2 + width < x1) {
         const path = `M ${x1} ${y1} L ${x2 + width} ${y2 + 0.1}`;
-        return path;
+        return { path, breakpoints };
       }
     }
     if (radius > 2) radius = 10 * zoom;
@@ -37,75 +41,63 @@ export function calcPath(r, tableWidth = 200, zoom = 1) {
 
   if (y1 <= y2) {
     if (x1 + width <= x2) {
-      const path = `M ${x1 + width} ${y1} L ${
-        midX - radius
-      } ${y1} A ${radius} ${radius} 0 0 1 ${midX} ${y1 + radius} L ${midX} ${
-        y2 - radius
-      } A ${radius} ${radius} 0 0 0 ${midX + radius} ${y2} L ${x2} ${y2}`;
-      return path;
+      breakpoints.push(
+        { x: midX, y: y1 + radius },
+        { x: midX, y: y2 - radius }
+      );
+      const path = `M ${x1 + width} ${y1} L ${midX - radius} ${y1} A ${radius} ${radius} 0 0 1 ${midX} ${y1 + radius} L ${midX} ${y2 - radius} A ${radius} ${radius} 0 0 0 ${midX + radius} ${y2} L ${x2} ${y2}`;
+      return { path, breakpoints };
     } else if (x2 <= x1 + width && x1 <= x2) {
-      const path = `M ${x1 + width} ${y1} L ${
-        x1 + width + radius
-      } ${y1} A ${radius} ${radius} 0 0 1 ${x1 + width + radius + radius} ${
-        y1 + radius
-      } L ${x1 + width + radius + radius} ${y2 - radius} A ${radius} ${radius} 0 0 1 ${
-        x1 + width + radius
-      } ${y2} L ${x2} ${y2}`;
-      return path;
+      breakpoints.push(
+        { x: x1 + width + radius + radius, y: y1 + radius },
+        { x: x1 + width + radius + radius, y: y2 - radius }
+      );
+      const path = `M ${x1 + width} ${y1} L ${x1 + width + radius} ${y1} A ${radius} ${radius} 0 0 1 ${x1 + width + radius + radius} ${y1 + radius} L ${x1 + width + radius + radius} ${y2 - radius} A ${radius} ${radius} 0 0 1 ${x1 + width + radius} ${y2} L ${x2} ${y2}`;
+      return { path, breakpoints };
     } else if (x2 + width >= x1 && x2 + width <= x1 + width) {
-      const path = `M ${x1} ${y1} L ${
-        x1 - radius
-      } ${y1} A ${radius} ${radius} 0 0 0 ${x1 - radius - radius} ${
-        y1 + radius
-      } L ${x1 - radius - radius} ${y2 - radius} A ${radius} ${radius} 0 0 0 ${
-        x1 - radius
-      } ${y2} L ${x2 + width} ${y2}`;
-      return path;
+      breakpoints.push(
+        { x: x1 - radius - radius, y: y1 + radius },
+        { x: x1 - radius - radius, y: y2 - radius }
+      );
+      const path = `M ${x1} ${y1} L ${x1 - radius} ${y1} A ${radius} ${radius} 0 0 0 ${x1 - radius - radius} ${y1 + radius} L ${x1 - radius - radius} ${y2 - radius} A ${radius} ${radius} 0 0 0 ${x1 - radius} ${y2} L ${x2 + width} ${y2}`;
+      return { path, breakpoints };
     } else {
-      const path = `M ${x1} ${y1} L ${
-        midX + radius
-      } ${y1} A ${radius} ${radius} 0 0 0 ${midX} ${y1 + radius} L ${midX} ${
-        y2 - radius
-      } A ${radius} ${radius} 0 0 1 ${midX - radius} ${y2} L ${x2 + width} ${y2}`;
-      return path;
+      breakpoints.push(
+        { x: midX, y: y1 + radius },
+        { x: midX, y: y2 - radius }
+      );
+      const path = `M ${x1} ${y1} L ${midX + radius} ${y1} A ${radius} ${radius} 0 0 0 ${midX} ${y1 + radius} L ${midX} ${y2 - radius} A ${radius} ${radius} 0 0 1 ${midX - radius} ${y2} L ${x2 + width} ${y2}`;
+      return { path, breakpoints };
     }
   } else {
     if (x1 + width <= x2) {
-      const path = `M ${x1 + width} ${y1} L ${
-        midX - radius
-      } ${y1} A ${radius} ${radius} 0 0 0 ${midX} ${y1 - radius} L ${midX} ${
-        y2 + radius
-      } A ${radius} ${radius} 0 0 1 ${midX + radius} ${y2} L ${x2} ${y2}`;
-      return path;
+      breakpoints.push(
+        { x: midX, y: y1 - radius },
+        { x: midX, y: y2 + radius }
+      );
+      const path = `M ${x1 + width} ${y1} L ${midX - radius} ${y1} A ${radius} ${radius} 0 0 0 ${midX} ${y1 - radius} L ${midX} ${y2 + radius} A ${radius} ${radius} 0 0 1 ${midX + radius} ${y2} L ${x2} ${y2}`;
+      return { path, breakpoints };
     } else if (x1 + width >= x2 && x1 + width <= x2 + width) {
-       const path = `M ${x1} ${y1} L ${
-        x1 - radius - radius
-      } ${y1} A ${radius} ${radius} 0 0 1 ${x1 - radius - radius - radius} ${
-        y1 - radius
-      } L ${x1 - radius - radius - radius} ${
-        y2 + radius
-      } A ${radius} ${radius} 0 0 1 ${
-        x1 - radius - radius
-      } ${y2} L ${x2 + width} ${y2}`;
-      return path;
+      breakpoints.push(
+        { x: x1 - radius - radius - radius, y: y1 - radius },
+        { x: x1 - radius - radius - radius, y: y2 + radius }
+      );
+      const path = `M ${x1} ${y1} L ${x1 - radius - radius} ${y1} A ${radius} ${radius} 0 0 1 ${x1 - radius - radius - radius} ${y1 - radius} L ${x1 - radius - radius - radius} ${y2 + radius} A ${radius} ${radius} 0 0 1 ${x1 - radius - radius} ${y2} L ${x2 + width} ${y2}`;
+      return { path, breakpoints };
     } else if (x1 >= x2 && x1 <= x2 + width) {
-      const path = `M ${x1 + width} ${y1} L ${
-        x1 + width + radius
-      } ${y1} A ${radius} ${radius} 0 0 0 ${x1 + width + radius + radius} ${
-        y1 - radius
-      } L ${x1 + width + radius + radius} ${
-        y2 + radius
-      } A ${radius} ${radius} 0 0 0 ${x1 + width + radius} ${y2} L ${
-        x2
-      } ${y2}`;
-      return path;
+      breakpoints.push(
+        { x: x1 + width + radius + radius, y: y1 - radius },
+        { x: x1 + width + radius + radius, y: y2 + radius }
+      );
+      const path = `M ${x1 + width} ${y1} L ${x1 + width + radius} ${y1} A ${radius} ${radius} 0 0 0 ${x1 + width + radius + radius} ${y1 - radius} L ${x1 + width + radius + radius} ${y2 + radius} A ${radius} ${radius} 0 0 0 ${x1 + width + radius} ${y2} L ${x2} ${y2}`;
+      return { path, breakpoints };
     } else {
-      const path = `M ${x1} ${y1} L ${
-        midX + radius
-      } ${y1} A ${radius} ${radius} 0 0 1 ${midX} ${y1 - radius} L ${midX} ${
-        y2 + radius
-      } A ${radius} ${radius} 0 0 0 ${midX - radius} ${y2} L ${x2 + width} ${y2}`;
-      return path;
+      breakpoints.push(
+        { x: midX, y: y1 - radius },
+        { x: midX, y: y2 + radius }
+      );
+      const path = `M ${x1} ${y1} L ${midX + radius} ${y1} A ${radius} ${radius} 0 0 1 ${midX} ${y1 - radius} L ${midX} ${y2 + radius} A ${radius} ${radius} 0 0 0 ${midX - radius} ${y2} L ${x2 + width} ${y2}`;
+      return { path, breakpoints };
     }
   }
 }
