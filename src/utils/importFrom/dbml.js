aa4d1f4724944e4a0d2150f8cf6bf8c671b18c2d
1ilit <1ilit@proton.me>
2025-06-22 23:27:29 +0400

    Fix defaults when importing from dbml (#501)

--- src/utils/importFrom/dbml.js
+++ src/utils/importFrom/dbml.js
@@ -28,7 +28,7 @@ export function fromDBML(src) {
         field.id = nanoid();
         field.name = column.name;
         field.type = column.type.type_name.toUpperCase();
-        field.default = column.dbdefault ?? "";
+        field.default = column.dbdefault?.value ?? "";
         field.check = "";
         field.primary = !!column.pk;
         field.unique = !!column.pk;
