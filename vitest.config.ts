435a45622ab7a6b0980bf1f3480d2ae6e87c2d97
akib22 <adnanakib75@gmail.com>
2025-10-10 01:41:13 +0600

    Add tests for fullscreen utility functions
    Issue: #635

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    environment: 'jsdom',
     setupFiles: ['./vitest.setup.js'],
     exclude: ['node_modules', 'dist', 'e2e', 'tests', 'cypress', '**/node_modules/**'],
     globals: true,
