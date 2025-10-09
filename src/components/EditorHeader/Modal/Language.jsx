c21e51c868688aea460fca87dc148d55abb76bd5
akib22 <adnanakib75@gmail.com>
2025-10-09 14:02:42 +0600

    Fix language change applies without confirmation in modal
    Issue: #632

--- src/components/EditorHeader/Modal/Language.jsx
+++ src/components/EditorHeader/Modal/Language.jsx
@@ -1,22 +1,21 @@
-import { useTranslation } from "react-i18next";
 import { useSettings } from "../../../hooks";
 import { languages } from "../../../i18n/i18n";
 
-export default function Language() {
+export default function Language({ language, setLanguage }) {
   const { settings } = useSettings();
-  const { i18n } = useTranslation();
+  const isDarkMode = settings.mode === "dark";
 
   return (
     <div className="grid grid-cols-4 md:grid-cols-2 gap-4">
       {languages.map((l) => (
         <button
           key={l.code}
-          onClick={() => i18n.changeLanguage(l.code)}
+          onClick={() => setLanguage(l.code)}
           className={`space-y-1 py-3 px-4 rounded-md border-2 ${
-            settings.mode === "dark"
+            isDarkMode
               ? "bg-zinc-700 hover:bg-zinc-600"
               : "bg-zinc-100 hover:bg-zinc-200"
-          } ${i18n.language === l.code ? "border-zinc-400" : "border-transparent"}`}
+          } ${language === l.code ? "border-zinc-400" : "border-transparent"}`}
         >
           <div className="flex justify-between items-center">
             <div className="font-semibold">{l.native_name}</div>
