8dba94aed35b5a820cf4f20bdcc0fdb958985c42
khushthecoder <chaudharikhush0711@gmail.com>
2025-11-16 02:19:13 +0530

    fix: prevent crash on invalid URI during JSON import (#561)

--- src/utils/validateSchema.js
+++ src/utils/validateSchema.js
@@ -1,10 +1,14 @@
 import { Validator } from "jsonschema";
 import { ddbSchema, jsonSchema } from "../data/schemas";
+import { safeUriValidator } from "./safeFormats";
+
+const validator = new Validator();
+validator.customFormats.uri = safeUriValidator;
 
 export function jsonDiagramIsValid(obj) {
-  return new Validator().validate(obj, jsonSchema).valid;
+  return validator.validate(obj, jsonSchema).valid;
 }
 
 export function ddbDiagramIsValid(obj) {
-  return new Validator().validate(obj, ddbSchema).valid;
+  return validator.validate(obj, ddbSchema).valid;
 }
