3fb93675214512fc88780610348607fb8619a306
thawancomt <thawancomt@gmail.com>
2026-01-18 13:18:52 +0000

    fix ( relationship label) : offset label on flat connections to avoid overlap

--- src/components/EditorCanvas/Relationship.jsx
+++ src/components/EditorCanvas/Relationship.jsx
@@ -88,6 +88,9 @@ export default function Relationship({ data }) {
     );
     cardinalityEndX = point2.x;
     cardinalityEndY = point2.y;
+    if (Math.abs(cardinalityStartY - cardinalityEndY) < labelFontSize) {
+      labelY -= labelFontSize
+    }
   }
 
   const edit = () => {
