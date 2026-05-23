2e48f7ef6e55a6b7575ba11b7a42a44d0c5f1362
1ilit <1ilit@proton.me>
2026-05-23 05:21:48 +0400

    Freshen up editor UI (#1022)
    * freshen up canvas elements
    
    * update sidepanel resize ui
    
    * move toolbar to the bottom of the screen
    
    * improve code / structure navigation

---
+++ src/icons/IconTable.jsx
@@ -0,0 +1,12 @@
+export default function IconTable() {
+  return (
+    <svg height="16" width="16" viewBox="0 0 24 24">
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        d="M5 2 L19 2 A4 4 0 0 1 23 6 L23 18 A4 4 0 0 1 19 22 L5 22 A4 4 0 0 1 1 18 L1 6 A4 4 0 0 1 5 2 Z M1 8 L23 8"
+      />
+    </svg>
+  );
+}
--- src/icons/index.js
+++ src/icons/index.js
@@ -1,3 +1,5 @@
 export { default as IconAddTable } from "./IconAddTable";
+export { default as IconTable } from "./IconTable";
+export { default as IconRelationship } from "./IconRelationship";
 export { default as IconAddArea } from "./IconAddArea";
 export { default as IconAddNote } from "./IconAddNote";
