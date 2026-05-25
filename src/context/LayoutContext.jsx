22b16e80ec8bd6f0f6f8fe28289708dd27eb74d4
x3rt <git@x3rt.dev>
2026-05-25 02:40:46 -0600

    add query param layout control

--- src/context/LayoutContext.jsx
+++ src/context/LayoutContext.jsx
@@ -1,19 +1,36 @@
 import { createContext, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 export const LayoutContext = createContext(null);
 
+const defaultLayout = {
+  header: true,
+  sidebar: true,
+  issues: true,
+  toolbar: true,
+  dbmlEditor: false,
+  readOnly: false,
+};
+
 export default function LayoutContextProvider({ children }) {
+  const [searchParams] = useSearchParams();
+
   const [layout, setLayout] = useState({
-    header: true,
-    sidebar: true,
-    issues: true,
-    toolbar: true,
-    dbmlEditor: false,
-    readOnly: false,
+    ...defaultLayout,
+    header: searchParams.get("hideHeader") !== "true",
+    sidebar: searchParams.get("hideSidebar") !== "true",
+    toolbar: searchParams.get("hideToolbar") !== "true",
   });
 
+  const forceReadOnly = searchParams.get("forceReadOnly") === "true";
+
   return (
-    <LayoutContext.Provider value={{ layout, setLayout }}>
+    <LayoutContext.Provider
+      value={{
+        layout: forceReadOnly ? { ...layout, readOnly: true } : layout,
+        setLayout,
+      }}
+    >
       {children}
     </LayoutContext.Provider>
   );
