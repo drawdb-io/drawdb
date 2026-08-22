2f9cf2304aab7bab25bfdadb4a307998dbc9b3b9
Lilit Musheghyan <1ilit@proton.me>
2026-08-22 21:39:20 +0400

    Remove warp

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -11,7 +11,6 @@ import oraclesql_icon from "../assets/oraclesql.png";
 import sql_server_icon from "../assets/sql-server.png";
 import discord from "../assets/discord.png";
 import github from "../assets/github.png";
-import warp from "../assets/warp.png";
 import screenshot from "../assets/screenshot.png";
 import FadeIn from "../animations/FadeIn";
 import axios from "axios";
@@ -104,30 +103,7 @@ export default function LandingPage() {
       {/* Learn more */}
       <div id="learn-more">
         <div className="bg-zinc-100 py-10 px-28 md:px-8">
-          {/* Supported by */}
-          <div className="text-center mb-16">
-            <div className="text-2xl md:text-xl font-bold text-sky-800 mb-8">
-              Supported by
-            </div>
-            <div>
-              <a
-                href="https://warp.dev/drawdb"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src={warp}
-                  alt="warp.dev"
-                  width={260}
-                  className="m-auto mb-4"
-                />
-                <div className="font-semibold text-lg md:text-base">
-                  Next-gen AI-powered intelligent terminal for all platforms
-                </div>
-              </a>
-            </div>
-          </div>
-          <div className="mt-16 w-[75%] text-center sm:w-full mx-auto shadow-xs rounded-2xl border p-6 bg-white space-y-3 mb-12">
+          <div className="w-[75%] text-center sm:w-full mx-auto shadow-xs rounded-2xl border p-6 bg-white space-y-3 mb-12">
             <div className="text-lg font-medium">
               Build diagrams with a few clicks, see the full picture, export SQL
               scripts, customize your editor, and more.
